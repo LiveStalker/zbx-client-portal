67be3a2fd4e4a143681917717ca23be9dd2848ca
Alexey Grebenshchikov <mi.aleksio@gmail.com>
2017-11-08 09:51:52 +0300

    Add simple css

--- zbx_client_portal/config/settings/base.py
+++ zbx_client_portal/config/settings/base.py
@@ -135,7 +135,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
+STATICFILES_DIRS = [
+    root('assets'),
+]
 
 # django.contrib.auth
 LOGIN_REDIRECT_URL = '/'
--- zbx_client_portal/portal/views.py
+++ zbx_client_portal/portal/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 
 def index(request):
+    if request.user.is_anonymous:
+        return redirect(reverse('userprofile:login'))
     return render(request, 'index.html')
